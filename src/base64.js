a3b060ec8f820aa8e43c23cec5cae55df7d913b8
Nameer Rizvi <rizvinameer@gmail.com>
2020-06-26 14:09:39 -0400

    base64 encode/decode json

--- src/base64.js
+++ src/base64.js
@@ -5,4 +5,6 @@ module.exports = {
     isStringValid(value) && Buffer.from(value).toString("base64"),
   decode: (base64) =>
     isStringValid(base64) && Buffer.from(base64, "base64").toString(),
+  encodeJSON: (json) => Buffer.from(JSON.stringify(json)).toString("base64"),
+  decodeJSON: (base64) => JSON.parse(Buffer.from(base64, "base64").toString()),
 };
