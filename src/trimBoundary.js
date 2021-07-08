edd97c44e1790a3a8825888584d3fcd4690583fb
Nameer Rizvi <rizvinameer@gmail.com>
2021-07-08 10:04:42 -0400

    trimBoundary enhancement

--- src/trimBoundary.js
+++ src/trimBoundary.js
@@ -1,14 +1,11 @@
 const trim = require("./trim");
 
-function trimBoundary(dirty, boundary = `"`, delimiter = " ") {
+function trimBoundary(dirty, boundary, delimiter = " ") {
   let clean = trim(dirty, delimiter);
 
-  if (
-    clean &&
-    boundary &&
-    clean.startsWith(boundary) &&
-    clean.endsWith(boundary)
-  )
+  if (!boundary) boundary = clean.charAt(0);
+
+  if (clean && clean.startsWith(boundary) && clean.endsWith(boundary))
     clean = clean.substring(1, clean.length - 1).trim();
 
   return clean;
