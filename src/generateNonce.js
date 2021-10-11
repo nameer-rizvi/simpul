16594e004c5ecd4c977a245acf9f18374bf91a26
Nameer Rizvi <rizvinameer@gmail.com>
2021-10-11 10:40:34 -0400

    require crypto to generate nonce

--- src/generateNonce.js
+++ src/generateNonce.js
@@ -1,4 +1,5 @@
 const tryCallback = require("./tryCallback");
+const crypto = require("crypto");
 
 const generateNonce = (callback) =>
   tryCallback(() => crypto.randomBytes(16).toString("hex"), callback);
