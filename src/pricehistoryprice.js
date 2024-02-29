86c707a6855be2da950cf199c3cd73162985f21b
nameer-rizvi <rizvinameer@gmail.com>
2024-02-29 16:40:02 -0500

    fixed leverage logic

--- src/pricehistoryprice.js
+++ src/pricehistoryprice.js
@@ -12,28 +12,34 @@ function pricehistoryprice(option, curr, candle, last) {
   if (option.leverage > 1) {
     let leveraged = {};
 
-    // Price Open.
-
     leveraged.priceOpen = last?.priceClose2
-      ? calcLeverage(last.priceClose2, candle.priceOpen, option.leverage)
+      ? calcLeverage(
+          last.priceClose2,
+          candle.priceOpen,
+          option.leverage,
+          last.priceClose,
+        )
       : candle.priceOpen;
 
     leveraged.priceHigh = calcLeverage(
       candle.priceOpen,
       candle.priceHigh,
       option.leverage,
+      leveraged.priceOpen,
     );
 
     leveraged.priceLow = calcLeverage(
       candle.priceOpen,
       candle.priceLow,
       option.leverage,
+      leveraged.priceOpen,
     );
 
     leveraged.priceClose = calcLeverage(
       candle.priceOpen,
       candle.priceClose,
       option.leverage,
+      leveraged.priceOpen,
     );
 
     candle.priceOpen = leveraged.priceOpen;
@@ -42,9 +48,9 @@ function pricehistoryprice(option, curr, candle, last) {
 
     candle.priceLow = leveraged.priceLow;
 
-    candle.priceClose = leveraged.priceClose;
-
     candle.priceClose2 = candle.priceClose;
+
+    candle.priceClose = leveraged.priceClose;
 
     if (last?.priceClose2) delete last.priceClose2;
   }
@@ -62,8 +68,8 @@ function pricehistoryprice(option, curr, candle, last) {
   }
 }
 
-function calcLeverage(num1, num2, lev) {
-  return math.num(num1 + num1 * math.change.percent(num1, num2) * lev);
+function calcLeverage(num1, num2, lev, num3) {
+  return math.num(num3 + num3 * (math.change.percent(num1, num2) * lev));
 }
 
 module.exports = pricehistoryprice;
