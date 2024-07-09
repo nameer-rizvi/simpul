b10a3d67df7a084f9c15520d3a3e14f746afb0d6
nameer-rizvi <rizvinameer@gmail.com>
2024-07-08 22:12:59 -0400

    revert recursively

--- src/recursively.js
+++ src/recursively.js
@@ -1,25 +1,25 @@
 const validate = require("./validate");
 
 function recursively(json, callback, depth = 0, path = []) {
-  if (!validate.isObject(json) && !validate.isArray(json)) {
-    const result = { key: path[0], path: path[1], value: json, depth };
-    return callback(result) || json;
-  }
-
   function makePath(curr) {
-    return [curr.toString(), path[1] ? `${path[1]}.${curr}` : curr.toString()];
+    const path0 = curr.toString();
+    const path1 = path?.[1] ? `${path[1]}.${curr}` : curr.toString();
+    return [path0, path1];
   }
 
   if (validate.isArray(json)) {
-    return json.map((item, index) => {
-      return recursively(item, callback, depth + 1, makePath(index));
-    });
+    for (let i = 0; i < json.length; i++)
+      json[i] = recursively(json[i], callback, depth + 1, makePath(i));
+    return json;
   }
 
-  return Object.keys(json).reduce((acc, key) => {
-    acc[key] = recursively(json[key], callback, depth + 1, makePath(key));
-    return acc;
-  }, {});
+  if (validate.isObject(json)) {
+    for (let key in json)
+      json[key] = recursively(json[key], callback, depth + 1, makePath(key));
+    return json;
+  }
+
+  return callback({ key: path[0], path: path[1], value: json, depth });
 }
 
 module.exports = recursively;
